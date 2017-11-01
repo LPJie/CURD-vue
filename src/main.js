a8c26c60df22b3f59c168268a501cc084d2063a3
lipengjie <158121260@qq.com>
2017-11-01 10:46:01 +0800

    项目安装

---
+++ src/main.js
@@ -0,0 +1,50 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//引入axios
+import axios from 'axios'
+//引入vuex
+import Vuex from 'vuex'
+//引入element-ui
+import ElementUi from 'element-ui'
+//引入element样式
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.config.productionTip = false
+
+//使用vuex
+Vue.use(Vuex)
+//使用element-ui
+Vue.use(ElementUi)
+//创建一个vuex的实例
+const store = new Vuex.Store({
+  //存放数据的地方
+  state:{
+
+  },
+  //getter也是存放数据的地方，但是这里跟state的区别主要在于getter中用于计算后的结果
+  getter:{
+
+  },
+  //用于存放状态的地方
+  mutations:{
+
+  },
+  //也是用于改变状态的地方，但是通常用于异步改变
+  actions:{
+
+  },
+  modules:{
+
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,//Vue-router路由
+  store, //vuex
+  template: '<App/>',
+  components: { App }
+})
---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <el-row>
+    <el-col :span="8"><div class="grid-content bg-purple">这样说</div></el-col>
+    <el-col :span="8"><div class="grid-content bg-purple-light">那样说</div></el-col>
+    <el-col :span="8"><div class="grid-content bg-purple">怎么说</div></el-col>
+  </el-row>
+</div>
+</template>
+<script>
+  export default{
+      name:'Home'
+  }
+</script>
